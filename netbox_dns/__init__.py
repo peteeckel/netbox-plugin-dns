0ea1fe68d27edafd88e93f8b1a79cd8601578596
Peter Eckel <pe-git@hindenburgring.com>
2023-05-02 21:49:30 +0200

    Prepared pre-release 0.18.0-beta1

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -1,13 +1,13 @@
 from extras.plugins import PluginConfig
 
-__version__ = "0.17.0"
+__version__ = "0.18.0-beta1"
 
 
 class DNSConfig(PluginConfig):
     name = "netbox_dns"
     verbose_name = "Netbox DNS"
     description = "Netbox DNS"
-    min_version = "3.5-beta2"
+    min_version = "3.5"
     version = __version__
     author = "Aurora Research Lab"
     author_email = "info@aurorabilisim.com"
