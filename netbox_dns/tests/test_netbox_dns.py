2ea0ef9b8d67672d871ae141d5d7bb3649554b5f
Peter Eckel <pe-git@hindenburgring.com>
2023-05-14 15:01:37 +0200

    Prepare release 0.18.2

--- netbox_dns/tests/test_netbox_dns.py
+++ netbox_dns/tests/test_netbox_dns.py
@@ -11,7 +11,7 @@ class NetboxDnsVersionTestCase(SimpleTestCase):
     """
 
     def test_version(self):
-        assert __version__ == "0.18.1"
+        assert __version__ == "0.18.2"
 
 
 class AppTest(APITestCase):
