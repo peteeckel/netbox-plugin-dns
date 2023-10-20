fcbb1921a6792fc52d1541c720b2261b19a87793
Peter Eckel <pe-git@hindenburgring.com>
2023-10-20 15:51:32 +0200

    Added IPAM prefixes to the View model
    * Added a many-to-many relation from ipam.Prefix to netbox_dns.View as a basis for automatic
      IPAM/NetBox DNS coupling
    * Added a new input field class for Prefixes to solve two problems:
      1. The labels NetBox automatically generates for Prefixes in selectors contain indentation, which
         is not suitable for drop down menus
      2. The same labels do not contain information about which VRF a prefix belongs to, if any.
      This was solved in part by rewriting the text within the input field class and providing a new
      API endpoint that does not return a "_depth" attribute so the frontend code won't try to render
      it with indentation characters.

--- netbox_dns/forms/view.py
+++ netbox_dns/forms/view.py
@@ -15,8 +15,10 @@ from utilities.forms import BOOLEAN_WITH_BLANK_CHOICES
 from utilities.forms.rendering import FieldSet
 from tenancy.models import Tenant
 from tenancy.forms import TenancyForm, TenancyFilterForm
+from ipam.models import Prefix
 
 from netbox_dns.models import View
+from netbox_dns.extras.fields import PrefixDynamicModelMultipleChoiceField
 
 
 __all__ = (
@@ -28,14 +30,28 @@ __all__ = (
 
 
 class ViewForm(TenancyForm, NetBoxModelForm):
+    prefixes = PrefixDynamicModelMultipleChoiceField(
+        queryset=Prefix.objects.all(),
+        required=False,
+        label="IPAM Prefixes",
+    )
+
     fieldsets = (
         FieldSet("name", "default_view", "description", "tags", name="View"),
         FieldSet("tenant_group", "tenant", name="Tenancy"),
+        FieldSet("prefixes", name="IPAM"),
     )
 
     class Meta:
         model = View
-        fields = ("name", "default_view", "description", "tags", "tenant")
+        fields = (
+            "name",
+            "default_view",
+            "description",
+            "tags",
+            "tenant",
+            "prefixes",
+        )
 
 
 class ViewFilterForm(TenancyFilterForm, NetBoxModelFilterSetForm):
@@ -77,6 +93,11 @@ class ViewBulkEditForm(NetBoxModelBulkEditForm):
 
     description = forms.CharField(max_length=200, required=False)
     tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+    prefixes = PrefixDynamicModelMultipleChoiceField(
+        queryset=Prefix.objects.all(),
+        required=False,
+        label="IPAM Prefixes",
+    )
 
     fieldsets = (
         FieldSet(
