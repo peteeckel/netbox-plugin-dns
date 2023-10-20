ce66c82b13186d47ed94dca5f68c558c38f5ea6b
Peter Eckel <pe-git@hindenburgring.com>
2023-10-20 15:51:32 +0200

    Added IPAM prefixes to the View model
    * Added a many-to-many relation from ipam.Prefix to netbox_dns.View as a basis for automatic
      IPAM/NetBox DNS coupling
    * Added a new input field class for Prefixes to solve two problems:
      1. The labels NetBox automatically generates for Prefixes in selectors contain indentation, which
         is not suitable for drop down menus
      2. The same labels do not contain information about which VRF a prefix belongs to, if any.
      This was solved in part by rewriting the text within the input field class and providing a new
      API endpoint that does not return a "_depth" attribute so the frontend code won't try to render
      it with indentation characters.

--- netbox_dns/models/view.py
+++ netbox_dns/models/view.py
@@ -29,6 +29,11 @@ class View(ObjectModificationMixin, ContactsMixin, NetBoxModel):
     default_view = models.BooleanField(
         default=False,
     )
+    prefixes = models.ManyToManyField(
+        to="ipam.Prefix",
+        related_name="netbox_dns_views",
+        blank=True,
+    )
     tenant = models.ForeignKey(
         to="tenancy.Tenant",
         on_delete=models.PROTECT,
