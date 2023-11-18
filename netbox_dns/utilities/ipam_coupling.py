c6c8859a11fdb93d45ef1b9dce9050c209dc6016
Peter Eckel <pe-git@hindenburgring.com>
2023-11-18 13:10:31 +0100

    Improved DNS record creation for IPAM coupling
    * Removed the middleware code as it turned out to be too limited for the purpose. More
      specifically, it is not used for model-level changes to objects, so it doesn't work
      with Custom Scripts etc. (see issue #105)
    * All checks and operations are now done in signal handlers for ipam.IPAddress that are
      part of NetBox DNS. Future releases (dropping support for NetBox < 3.7) could be
      using CUSTOM_VALIDATORS and PROTECTION_RULES instead.
    * Testing was extended to cover model-level actions and API level actions to IPAddress
      objects.
    * Improved DNS record permission checks.
    
    Open issue:
    
    * Although actions with missing permissions to delete DNS records work properly in the
      API and the GUI, the respective tests have to be skipped as they fail in the context
      of unit testing. Potential reasons include a bug in the test framework. This does not
      have any functional implications and just affects tests for the deletion of IPAddress
      objects with missing NetBox DNS (object) permissions.

---
+++ netbox_dns/utilities/ipam_coupling.py
@@ -0,0 +1,87 @@
+from ipam.choices import IPAddressStatusChoices
+from utilities.permissions import resolve_permission
+
+from netbox_dns.models import Record, RecordTypeChoices, RecordStatusChoices
+
+
+class DNSPermissionDenied(Exception):
+    pass
+
+
+def ipaddress_cf_data(ip_address):
+    name = ip_address.custom_field_data.get("ipaddress_dns_record_name")
+    zone_id = ip_address.custom_field_data.get("ipaddress_dns_zone_id")
+
+    if name is None or zone_id is None:
+        return None, None
+
+    return name, zone_id
+
+
+def address_record_type(ip_address):
+    return RecordTypeChoices.AAAA if ip_address.family == 6 else RecordTypeChoices.A
+
+
+def address_record_status(ip_address):
+    return (
+        RecordStatusChoices.STATUS_ACTIVE
+        if ip_address.status == IPAddressStatusChoices.STATUS_ACTIVE
+        else RecordStatusChoices.STATUS_INACTIVE
+    )
+
+
+def get_address_record(ip_address):
+    return ip_address.netbox_dns_records.first()
+
+
+def new_address_record(instance):
+    name, zone_id = ipaddress_cf_data(instance)
+
+    if zone_id is None:
+        return None
+
+    return Record(
+        name=name,
+        zone_id=zone_id,
+        status=address_record_status(instance),
+        type=address_record_type(instance),
+        value=str(instance.address.ip),
+        ipam_ip_address_id=instance.id,
+        managed=True,
+    )
+
+
+def update_address_record(record, ip_address):
+    name, zone_id = ipaddress_cf_data(ip_address)
+
+    record.name = name
+    record.zone_id = zone_id
+    record.status = address_record_status(ip_address)
+    record.value = str(ip_address.address.ip)
+
+
+def check_permission(request, permission, record=None):
+    if record is not None and record.pk is None:
+        check_record = None
+    else:
+        check_record = record
+
+    user = request.user
+
+    if not user.has_perm(permission, check_record):
+        action = resolve_permission(permission)[1]
+        item = "records" if check_record is None else f"record {check_record}"
+
+        raise DNSPermissionDenied(f"User {user} is not allowed to {action} DNS {item}")
+
+
+def dns_changed(old, new):
+    return any(
+        (
+            old.address.ip != new.address.ip,
+            old.custom_field_data["ipaddress_dns_record_name"]
+            != new.custom_field_data["ipaddress_dns_record_name"],
+            old.custom_field_data["ipaddress_dns_zone_id"]
+            != new.custom_field_data["ipaddress_dns_zone_id"],
+        )
+    )
