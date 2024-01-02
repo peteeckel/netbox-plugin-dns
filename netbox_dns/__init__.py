8a5f39d6b8b85a8d9a78505cad239bfbff5f78b2
Peter Eckel <pe-git@hindenburgring.com>
2024-01-02 21:20:28 +0100

    Prepared release 0.21.11

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -9,7 +9,7 @@ except ImportError:
     # NetBox 3.5.8
     from extras.plugins.utils import get_plugin_config
 
-__version__ = "0.21.10"
+__version__ = "0.21.11"
 
 
 class DNSConfig(PluginConfig):
