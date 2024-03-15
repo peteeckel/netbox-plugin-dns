d568f5810d5af811f7b4a02d98ebcb89d630875c
Peter Eckel <pe-git@hindenburgring.com>
2024-03-15 16:15:22 +0100

    Created management command for setting TTLs of RRSets to unique values

---
+++ netbox_dns/management/commands/cleanup_rrset_ttl.py
@@ -0,0 +1,60 @@
+from django.core.management.base import BaseCommand
+from django.db.models import Max, Min
+
+from netbox_dns.models import (
+    Record,
+    RecordTypeChoices,
+)
+
+
+class Command(BaseCommand):
+    help = "Clean up the TTLs for RRSets"
+
+    def add_arguments(self, parser):
+        min_max = parser.add_mutually_exclusive_group()
+        min_max.add_argument(
+            "--min",
+            action="store_true",
+            help="Use the minimum TTL of an RRSet for all Records",
+        )
+        min_max.add_argument(
+            "--max",
+            action="store_true",
+            help="Use the maximum TTL of an RRSet for all Records",
+        )
+
+    def handle(self, *model_names, **options):
+        self.cleanup_rrset_ttl(**options)
+
+        self.stdout.write("RRSet cleanup completed.")
+
+    def cleanup_rrset_ttl(self, **options):
+        verbose = options.get("verbosity") > 1
+
+        ttl_records = Record.objects.filter(ttl__isnull=False).exclude(
+            type=RecordTypeChoices.SOA
+        )
+        for record in ttl_records:
+            records = Record.objects.filter(
+                name=record.name,
+                zone=record.zone,
+                type=record.type,
+            )
+
+            if records.count() == 1:
+                if options.get("verbosity") > 2:
+                    self.stdout.write(f"Ignoring single record {record.id} ({record})")
+                continue
+
+            if options.get("max"):
+                ttl = records.aggregate(Max("ttl")).get("ttl__max")
+            else:
+                ttl = records.aggregate(Min("ttl")).get("ttl__min")
+
+            for record in records.exclude(ttl=ttl):
+                if options.get("verbosity") > 1:
+                    self.stdout.write(
+                        f"Updating TTL for record {record.id} ({record}) to {ttl}"
+                    )
+                record.ttl = ttl
+                record.save(update_fields=["ttl"], update_rrset_ttl=False)
