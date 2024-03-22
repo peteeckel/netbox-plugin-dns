0d9a2183ceb2a23962f48d0a9d3507b50ab502e6
Peter Eckel <pe-git@hindenburgring.com>
2024-03-23 00:42:10 +0100

    Prepared release 0.22.6

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -10,7 +10,7 @@ except ImportError:
     # NetBox 3.5.8
     from extras.plugins.utils import get_plugin_config
 
-__version__ = "0.22.5"
+__version__ = "0.22.6"
 
 
 class DNSConfig(PluginConfig):
--- netbox_dns/tests/test_netbox_dns.py
+++ netbox_dns/tests/test_netbox_dns.py
@@ -7,7 +7,7 @@ from netbox_dns.tests.custom import APITestCase
 
 class NetBoxDNSVersionTestCase(SimpleTestCase):
     def test_version(self):
-        assert __version__ == "0.22.5"
+        assert __version__ == "0.22.6"
 
 
 class AppTest(APITestCase):
