9661b33dd1bb9c54fe749b4fbb447ce56b279d49
Peter Eckel <pe-git@hindenburgring.com>
2024-05-02 16:55:23 +0200

    Prepared version 1.0-beta2

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -1,14 +1,13 @@
 from netbox.plugins import PluginConfig
 
-__version__ = "0.22.9"
+__version__ = "1.0-beta2"
 
 
 class DNSConfig(PluginConfig):
     name = "netbox_dns"
     verbose_name = "NetBox DNS"
     description = "NetBox plugin for DNS data"
-    min_version = "3.5.0"
-    max_version = "3.99.0"
+    min_version = "4.0-beta1"
     version = __version__
     author = "Peter Eckel"
     author_email = "pete@netbox-dns.org"
