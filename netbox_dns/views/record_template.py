201d745e2cdd0467e80e7a9a28aeb187c8a46c3c
Peter Eckel <pe-git@hindenburgring.com>
2024-07-12 15:51:12 +0200

    Added views for the templating models

---
+++ netbox_dns/views/record_template.py
@@ -0,0 +1,79 @@
+from dns import name as dns_name
+
+from netbox.views import generic
+
+from netbox_dns.filtersets import RecordTemplateFilterSet
+from netbox_dns.forms import (
+    RecordTemplateImportForm,
+    RecordTemplateFilterForm,
+    RecordTemplateForm,
+    RecordTemplateBulkEditForm,
+)
+from netbox_dns.models import RecordTemplate
+from netbox_dns.tables import RecordTemplateTable
+from netbox_dns.utilities import value_to_unicode
+
+
+__ALL__ = (
+    "RecordTemplateListView",
+    "RecordTemplateView",
+    "RecordTemplateEditView",
+    "RecordTemplateDeleteView",
+    "RecordTemplateBulkImportView",
+    "RecordTemplateBulkEditView",
+    "RecordTemplateBulkDeleteView",
+)
+
+
+class RecordTemplateListView(generic.ObjectListView):
+    queryset = RecordTemplate.objects.all()
+    filterset = RecordTemplateFilterSet
+    filterset_form = RecordTemplateFilterForm
+    table = RecordTemplateTable
+
+
+class RecordTemplateView(generic.ObjectView):
+    queryset = RecordTemplate.objects.all()
+
+    def get_extra_context(self, request, instance):
+        context = {}
+
+        name = dns_name.from_text(instance.record_name, origin=None)
+        if name.to_text() != name.to_unicode():
+            context["unicode_name"] = name.to_unicode()
+
+        unicode_value = value_to_unicode(instance.value)
+        if instance.value != unicode_value:
+            context["unicode_value"] = unicode_value
+
+        return context
+
+
+class RecordTemplateEditView(generic.ObjectEditView):
+    queryset = RecordTemplate.objects.all()
+    form = RecordTemplateForm
+    default_return_url = "plugins:netbox_dns:recordtemplate_list"
+
+
+class RecordTemplateDeleteView(generic.ObjectDeleteView):
+    queryset = RecordTemplate.objects.all()
+    default_return_url = "plugins:netbox_dns:recordtemplate_list"
+
+
+class RecordTemplateBulkImportView(generic.BulkImportView):
+    queryset = RecordTemplate.objects.all()
+    model_form = RecordTemplateImportForm
+    table = RecordTemplateTable
+    default_return_url = "plugins:netbox_dns:recordtemplate_list"
+
+
+class RecordTemplateBulkEditView(generic.BulkEditView):
+    queryset = RecordTemplate.objects.all()
+    filterset = RecordTemplateFilterSet
+    table = RecordTemplateTable
+    form = RecordTemplateBulkEditForm
+
+
+class RecordTemplateBulkDeleteView(generic.BulkDeleteView):
+    queryset = RecordTemplate.objects.all()
+    table = RecordTemplateTable
