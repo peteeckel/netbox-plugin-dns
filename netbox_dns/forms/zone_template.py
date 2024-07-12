732c6306d061949972356d7e4b1ea49f4ca28543
Peter Eckel <pe-git@hindenburgring.com>
2024-07-12 16:24:27 +0200

    Added forms for zone templating

---
+++ netbox_dns/forms/zone_template.py
@@ -0,0 +1,281 @@
+from django import forms
+
+from netbox.forms import (
+    NetBoxModelBulkEditForm,
+    NetBoxModelFilterSetForm,
+    NetBoxModelImportForm,
+    NetBoxModelForm,
+)
+from utilities.forms.fields import (
+    DynamicModelMultipleChoiceField,
+    TagFilterField,
+    CSVModelChoiceField,
+    CSVModelMultipleChoiceField,
+    DynamicModelChoiceField,
+)
+from utilities.forms.widgets import BulkEditNullBooleanSelect, APISelect
+from utilities.forms.rendering import FieldSet
+from tenancy.models import Tenant
+from tenancy.forms import TenancyForm, TenancyFilterForm
+
+from netbox_dns.models import ZoneTemplate, NameServer, Registrar, Contact
+
+
+__ALL__ = (
+    "ZoneTemplateForm",
+    "ZoneTemplateFilterForm",
+    "ZoneTemplateImportForm",
+    "ZoneTemplateBulkEditForm",
+)
+
+
+class ZoneTemplateForm(TenancyForm, NetBoxModelForm):
+    nameservers = DynamicModelMultipleChoiceField(
+        queryset=NameServer.objects.all(),
+        required=False,
+    )
+
+    fieldsets = (
+        FieldSet("name", "description", "nameservers", name="Zone Template"),
+        FieldSet(
+            "registrar",
+            "registrant",
+            "admin_c",
+            "tech_c",
+            "billing_c",
+            name="Domain Registration",
+        ),
+        FieldSet("tags", name="Tags"),
+        FieldSet("tenant_group", "tenant", name="Tenancy"),
+    )
+
+    class Meta:
+        model = ZoneTemplate
+
+        fields = (
+            "name",
+            "nameservers",
+            "description",
+            "tags",
+            "registrar",
+            "registrant",
+            "admin_c",
+            "tech_c",
+            "billing_c",
+            "tenant",
+        )
+
+
+class ZoneTemplateFilterForm(TenancyFilterForm, NetBoxModelFilterSetForm):
+    model = ZoneTemplate
+    fieldsets = (
+        FieldSet("q", "filter_id", "tag"),
+        FieldSet("name", "nameservers", "description", name="Attributes"),
+        FieldSet(
+            "registrar_id",
+            "registrant_id",
+            "admin_c_id",
+            "tech_c_id",
+            "billing_c_id",
+            name="Registration",
+        ),
+        FieldSet("tenant_group_id", "tenant_id", name="Tenancy"),
+    )
+
+    name = forms.CharField(
+        required=False,
+        label="Template name",
+    )
+    nameservers = DynamicModelMultipleChoiceField(
+        queryset=NameServer.objects.all(),
+        required=False,
+    )
+    description = forms.CharField(
+        required=False,
+    )
+    registrar_id = DynamicModelMultipleChoiceField(
+        queryset=Registrar.objects.all(),
+        required=False,
+        label="Registrar",
+    )
+    registrant_id = DynamicModelMultipleChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Registrant",
+    )
+    admin_c_id = DynamicModelMultipleChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Admin-C",
+    )
+    tech_c_id = DynamicModelMultipleChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Tech-C",
+    )
+    billing_c_id = DynamicModelMultipleChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Billing-C",
+    )
+    tag = TagFilterField(ZoneTemplate)
+
+
+class ZoneTemplateImportForm(NetBoxModelImportForm):
+    nameservers = CSVModelMultipleChoiceField(
+        queryset=NameServer.objects.all(),
+        to_field_name="name",
+        required=False,
+        help_text="Name servers for the zone",
+    )
+    registrar = CSVModelChoiceField(
+        queryset=Registrar.objects.all(),
+        required=False,
+        to_field_name="name",
+        help_text="Registrar the domain is registered with",
+        error_messages={
+            "invalid_choice": "Registrar not found.",
+        },
+    )
+    registrant = CSVModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        to_field_name="contact_id",
+        help_text="Owner of the domain",
+        error_messages={
+            "invalid_choice": "Registrant contact ID not found",
+        },
+    )
+    admin_c = CSVModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        to_field_name="contact_id",
+        help_text="Administrative contact for the domain",
+        error_messages={
+            "invalid_choice": "Administrative contact ID not found",
+        },
+    )
+    tech_c = CSVModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        to_field_name="contact_id",
+        help_text="Technical contact for the domain",
+        error_messages={
+            "invalid_choice": "Technical contact ID not found",
+        },
+    )
+    billing_c = CSVModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        to_field_name="contact_id",
+        help_text="Billing contact for the domain",
+        error_messages={
+            "invalid_choice": "Billing contact ID not found",
+        },
+    )
+    tenant = CSVModelChoiceField(
+        queryset=Tenant.objects.all(),
+        required=False,
+        to_field_name="name",
+        help_text="Assigned tenant",
+    )
+
+    class Meta:
+        model = ZoneTemplate
+
+        fields = (
+            "name",
+            "nameservers",
+            "description",
+            "registrar",
+            "registrant",
+            "admin_c",
+            "tech_c",
+            "billing_c",
+            "tenant",
+            "tags",
+        )
+
+
+class ZoneTemplateBulkEditForm(NetBoxModelBulkEditForm):
+    nameservers = DynamicModelMultipleChoiceField(
+        queryset=NameServer.objects.all(),
+        required=False,
+    )
+    description = forms.CharField(max_length=200, required=False)
+    registrar = DynamicModelChoiceField(
+        queryset=Registrar.objects.all(),
+        required=False,
+        widget=APISelect(
+            attrs={
+                "data-url": reverse_lazy("plugins-api:netbox_dns-api:registrar-list")
+            }
+        ),
+    )
+    registrant = DynamicModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_dns-api:contact-list")}
+        ),
+    )
+    admin_c = DynamicModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Administrative Contact",
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_dns-api:contact-list")}
+        ),
+    )
+    tech_c = DynamicModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Technical Contact",
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_dns-api:contact-list")}
+        ),
+    )
+    billing_c = DynamicModelChoiceField(
+        queryset=Contact.objects.all(),
+        required=False,
+        label="Billing Contact",
+        widget=APISelect(
+            attrs={"data-url": reverse_lazy("plugins-api:netbox_dns-api:contact-list")}
+        ),
+    )
+    tenant = CSVModelChoiceField(
+        queryset=Tenant.objects.all(),
+        required=False,
+        to_field_name="name",
+        help_text="Assigned tenant",
+    )
+    tenant = DynamicModelChoiceField(queryset=Tenant.objects.all(), required=False)
+
+    model = ZoneTemplate
+
+    fieldsets = (
+        FieldSet(
+            "nameservers",
+            "description",
+            name="Attributes",
+        ),
+        FieldSet(
+            "registrar",
+            "registrant",
+            "admin_c",
+            "tech_c",
+            "billing_c",
+            name="Domain Registration",
+        ),
+        FieldSet("tenant_group", "tenant", name="Tenancy"),
+    )
+
+    nullable_fields = (
+        "description",
+        "nameservers",
+        "registrar",
+        "registrant",
+        "admin_c",
+        "tech_c",
+        "billing_c",
+    )
