501342ce9da583a76c5d8089d4deaddebad26720
Peter Eckel <pe-git@hindenburgring.com>
2024-07-13 13:39:08 +0200

    Backward compatibility for existing scripts

--- netbox_dns/models/__init__.py
+++ netbox_dns/models/__init__.py
@@ -5,4 +5,9 @@ from .view import *
 from .contact import *
 from .registrar import *
 
+# +
+# Backwards compatibility fix, will be removed in version 1.1
+# -
+from netbox_dns.choices import *
+
 from netbox_dns.signals import ipam_coupling
