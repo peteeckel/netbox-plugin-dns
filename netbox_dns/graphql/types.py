e980c59a0ec08338b8d94c1a4470e8fd4524d075
Peter Eckel <pe-git@hindenburgring.com>
2024-07-24 11:46:32 +0200

    Added prefixes per view to the API

--- netbox_dns/graphql/types.py
+++ netbox_dns/graphql/types.py
@@ -40,6 +40,7 @@ class NetBoxDNSViewType(NetBoxObjectType):
     name: str
     description: str
     tenant: Annotated["TenantType", strawberry.lazy("tenancy.graphql.types")] | None
+    prefixes: List[Annotated["PrefixType", strawberry.lazy("ipam.graphql.types")]]
 
 
 @strawberry_django.type(Zone, fields="__all__", filters=NetBoxDNSZoneFilter)
