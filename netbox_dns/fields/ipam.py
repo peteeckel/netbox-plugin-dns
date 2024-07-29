866a5f08f4aa88e400714cfbdcbcfd9cf7a039d7
Peter Eckel <pe-git@hindenburgring.com>
2024-07-29 21:22:54 +0200

    Adjusted serializers, fields and views

--- netbox_dns/fields/ipam.py
+++ netbox_dns/fields/ipam.py
@@ -2,16 +2,17 @@ from utilities.forms.fields import DynamicModelMultipleChoiceField
 from utilities.forms.widgets import APISelectMultiple
 
 
+__all__ = ("PrefixDynamicModelMultipleChoiceField",)
+
+
 class PrefixDynamicModelMultipleChoiceField(DynamicModelMultipleChoiceField):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    widget = APISelectMultiple(
-        api_url='/api/plugins/netbox-dns/prefixes'
-    )
+    widget = APISelectMultiple(api_url="/api/plugins/netbox-dns/prefixes")
 
     def label_from_instance(self, obj):
         if obj.vrf:
-            return f'{str(obj.prefix)} [{obj.vrf.name}]'
+            return f"{str(obj.prefix)} [{obj.vrf.name}]"
 
         return str(obj.prefix)
