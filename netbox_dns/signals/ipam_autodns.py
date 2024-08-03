cb7bee6df0e97c1d78082f6ddaa08e50dec257e3
Peter Eckel <pe-git@hindenburgring.com>
2024-08-03 19:59:07 +0200

    Worked around some weird issues with mwm_changed and pre_delete handlers

--- netbox_dns/signals/ipam_autodns.py
+++ netbox_dns/signals/ipam_autodns.py
@@ -7,11 +7,13 @@ from django.core.exceptions import ValidationError
 from netbox.context import current_request
 from netbox.signals import post_clean
 from ipam.models import IPAddress, Prefix
+from utilities.exceptions import AbortRequest
 
-from netbox_dns.models import View
+from netbox_dns.models import view as _view
 from netbox_dns.utilities import (
     update_dns_records,
     delete_dns_records,
+    get_views_by_prefix,
     get_ip_addresses_by_prefix,
     get_ip_addresses_by_view,
 )
@@ -60,30 +62,67 @@ def ipam_autodns_prefix_pre_save(instance, **kwargs):
 
 @receiver(pre_delete, sender=Prefix)
 def ipam_autodns_prefix_pre_delete(instance, **kwargs):
-    """
-    Workaround for an ancient Django bug (https://code.djangoproject.com/ticket/17688)
-    """
-    # +
-    # If the deletion happens via API or GUI, there is a request object and
-    # NetBox handles this itself as part of handle_deleted_object() in
-    # netbox/extras/signals.py, so we don't need to do anything here to avoid
-    # triggering m2m_changed multiple times.
-    # -
-    if current_request.get() is not None:
-        return
+    parent = instance.get_parents().last()
+    request = current_request.get()
+
+    if parent is not None and get_views_by_prefix(instance) != get_views_by_prefix(
+        parent
+    ):
+        try:
+            for prefix in instance.get_children().filter(
+                _depth=instance.depth + 1, netbox_dns_views__isnull=True
+            ):
+                for ip_address in get_ip_addresses_by_prefix(prefix):
+                    update_dns_records(ip_address, commit=False)
+        except ValidationError as exc:
+            if request is not None:
+                raise AbortRequest(
+                    f"Prefix deletion would cause DNS errors: {exc.messages[0]} "
+                    "Please review DNS View assignments for this and the parent prefix"
+                )
+            else:
+                raise exc
 
     # +
-    # Explicitly remove the relation so the m2m_changed signal is sent by the
-    # View model relation to Prefix
+    # CAUTION: This only works because the NetBox workaround for an ancient
+    # Django bug (see https://code.djangoproject.com/ticket/17688) has already
+    # removed the relations between the prefix and the views when this signal
+    # handler runs.
+    #
+    # Should anything be fixed, this code will stop working and need to be
+    # revisited.
+    #
+    # The NetBox workaround only works for requests, not for model level
+    # operations. The following code replicates it for non-requests.
     # -
-    for view in instance.netbox_dns_views.all():
-        view.snapshot()
-        view.prefixes.remove(instance)
+    if request is None:
+        for view in instance.netbox_dns_views.all():
+            view.snapshot()
+            view.prefixes.remove(instance)
+
+    for ip_address in get_ip_addresses_by_prefix(instance):
+        update_dns_records(ip_address)
 
 
-@receiver(m2m_changed, sender=View.prefixes.through)
+@receiver(m2m_changed, sender=_view.View.prefixes.through)
 def ipam_autodns_view_prefix_changed(**kwargs):
-    if kwargs.get("action") not in ("post_add", "post_remove"):
+    action = kwargs.get("action")
+    request = current_request.get()
+
+    # +
+    # Handle all post_add and post_remove signals except the ones directly
+    # handled by the pre_delete handler for the Prefix model.
+    #
+    # Yes. This IS ugly.
+    # -
+    if action not in ("post_add", "post_remove") or (
+        request is not None
+        and action == "post_remove"
+        and (
+            request.path.startswith("/ipam/prefixes/")
+            or request.path.startswith("/api/ipam/prefixes/")
+        )
+    ):
         return
 
     for prefix in Prefix.objects.filter(pk__in=kwargs.get("pk_set")):
