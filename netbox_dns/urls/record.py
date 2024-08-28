fcd02e6abdb54ebbe1e29c1f67c72baf5dc4aeec
Thomas Davis <tdavis@nersc.gov>
2024-08-28 09:52:15 -0700

    another try.

--- netbox_dns/urls/record.py
+++ netbox_dns/urls/record.py
@@ -44,6 +44,6 @@ record_urlpatterns = [
         "records/<int:pk>/contacts/",
         RecordContactView.as_view(),
         name="record_contacts",
-        kwargs={"contact": Record},
+        kwargs={"contacts": Record},
     ),
 ]
