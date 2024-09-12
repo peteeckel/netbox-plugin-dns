5569deacc42e7eaa1593d0e7ddb43d99d557c2c1
Peter Eckel <pe-git@hindenburgring.com>
2024-09-12 21:12:26 +0200

    Prepared release 1.1.1

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -5,7 +5,7 @@ from ipam.choices import IPAddressStatusChoices
 
 from netbox_dns.choices import RecordTypeChoices
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 
 class DNSConfig(PluginConfig):
