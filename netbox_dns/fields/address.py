177d7bf64bd67d79a469865e4d81e6ca15ec1d8f
Peter Eckel <pe-git@hindenburgring.com>
2024-09-29 15:17:55 +0200

    Added gettext support to fields

--- netbox_dns/fields/address.py
+++ netbox_dns/fields/address.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import models
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from netaddr import AddrFormatError, IPAddress
 
@@ -28,7 +29,7 @@ class AddressFormField(forms.Field):
 
 
 class AddressField(models.Field):
-    description = "IPv4/v6 address"
+    description = _("IPv4/v6 address")
 
     def python_type(self):
         return IPAddress
