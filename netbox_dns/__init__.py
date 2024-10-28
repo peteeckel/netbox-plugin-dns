234938340cf97ea59b96f8177aadd7192cb9a4c8
Peter Eckel <pe-git@hindenburgring.com>
2024-10-28 23:52:32 +0100

    Prepared release 1.1.5

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -7,7 +7,7 @@ from ipam.choices import IPAddressStatusChoices
 
 from netbox_dns.choices import RecordTypeChoices, RecordStatusChoices, ZoneStatusChoices
 
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 
 def _check_list(setting):
