97d2cfbe1a2965ed6907de42376f6d180c9ecb87
Peter Eckel <pe-git@hindenburgring.com>
2025-03-04 21:21:04 +0100

    Fixed API, Forms and Filtersets for DNSSECKeyTemplate

--- netbox_dns/filtersets/dnssec_key_template.py
+++ netbox_dns/filtersets/dnssec_key_template.py
@@ -1,19 +1,44 @@
+import django_filters
 from django.db.models import Q
 from django.utils.translation import gettext as _
 
 from netbox.filtersets import NetBoxModelFilterSet
 from tenancy.filtersets import TenancyFilterSet
 
-from netbox_dns.models import DNSSECKeyTemplate
+from netbox_dns.models import DNSSECKeyTemplate, DNSSECPolicy
+from netbox_dns.choices import (
+    DNSSECKeyTemplateTypeChoices,
+    DNSSECKeyTemplateAlgorithmChoices,
+)
 
 
 __all__ = ("DNSSECKeyTemplateFilterSet",)
 
 
 class DNSSECKeyTemplateFilterSet(TenancyFilterSet, NetBoxModelFilterSet):
+    type = django_filters.MultipleChoiceFilter(
+        choices=DNSSECKeyTemplateTypeChoices,
+    )
+    algorithm = django_filters.MultipleChoiceFilter(
+        choices=DNSSECKeyTemplateAlgorithmChoices,
+    )
+
+    dnssec_policy_id = django_filters.ModelMultipleChoiceFilter(
+        field_name="policies",
+        queryset=DNSSECPolicy.objects.all(),
+        to_field_name="id",
+        label=_("DNSSEC Policies"),
+    )
+    dnssec_policy = django_filters.ModelMultipleChoiceFilter(
+        field_name="policies__name",
+        queryset=DNSSECPolicy.objects.all(),
+        to_field_name="name",
+        label=_("DNSSEC Policies"),
+    )
+
     class Meta:
         model = DNSSECKeyTemplate
-        fields = ("id", "name", "description")
+        fields = ("id", "name", "description", "key_size", "lifetime")
 
     def search(self, queryset, name, value):
         if not value.strip():
