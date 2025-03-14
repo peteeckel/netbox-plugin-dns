4fcf22c9bd06c954825d942b521bce8eb013ff38
Peter Eckel <pe-git@hindenburgring.com>
2025-03-14 20:02:31 +0100

    Added NetBox 4.3 GraphQL support for DNSSEC models

--- netbox_dns/graphql/enums.py
+++ netbox_dns/graphql/enums.py
@@ -5,6 +5,11 @@ from netbox_dns.choices import (
     RecordClassChoices,
     RecordStatusChoices,
     ZoneStatusChoices,
+    DNSSECPolicyDigestChoices,
+    DNSSECPolicyStatusChoices,
+    DNSSECKeyTemplateTypeChoices,
+    DNSSECKeyTemplateAlgorithmChoices,
+    DNSSECKeyTemplateKeySizeChoices,
 )
 
 __all__ = (
@@ -12,9 +17,25 @@ __all__ = (
     "NetBoxDNSRecordClassEnum",
     "NetBoxDNSRecordStatusEnum",
     "NetBoxDNSZoneStatusEnum",
+    "NetBoxDNSDNSSECPolicyDigestEnum",
+    "NetBoxDNSDNSSECPolicyStatusEnum",
+    "NetBoxDNSDNSSECKeyTemplateTypeEnum",
+    "NetBoxDNSDNSSECKeyTemplateAlgorithmEnum",
+    "NetBoxDNSDNSSECKeyTemplateKeySizeEnum",
 )
 
 NetBoxDNSRecordTypeEnum = strawberry.enum(RecordTypeChoices.as_enum())
 NetBoxDNSRecordClassEnum = strawberry.enum(RecordClassChoices.as_enum())
 NetBoxDNSRecordStatusEnum = strawberry.enum(RecordStatusChoices.as_enum())
 NetBoxDNSZoneStatusEnum = strawberry.enum(ZoneStatusChoices.as_enum())
+NetBoxDNSDNSSECPolicyDigestEnum = strawberry.enum(DNSSECPolicyDigestChoices.as_enum())
+NetBoxDNSDNSSECPolicyStatusEnum = strawberry.enum(DNSSECPolicyStatusChoices.as_enum())
+NetBoxDNSDNSSECKeyTemplateTypeEnum = strawberry.enum(
+    DNSSECKeyTemplateTypeChoices.as_enum()
+)
+NetBoxDNSDNSSECKeyTemplateAlgorithmEnum = strawberry.enum(
+    DNSSECKeyTemplateAlgorithmChoices.as_enum()
+)
+NetBoxDNSDNSSECKeyTemplateKeySizeEnum = strawberry.enum(
+    DNSSECKeyTemplateKeySizeChoices.as_enum()
+)
