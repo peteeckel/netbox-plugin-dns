dcc3b6407c364a5dcba44543705954c7605871f2
Peter Eckel <pe-git@hindenburgring.com>
2025-04-30 12:59:18 +0200

    Prepared release 1.2.10

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 from netbox.plugins import PluginConfig
 from netbox.plugins.utils import get_plugin_config
 
-__version__ = "1.2.9"
+__version__ = "1.2.10"
 
 
 def _check_list(setting):
--- netbox_dns/tests/test_netbox_dns.py
+++ netbox_dns/tests/test_netbox_dns.py
@@ -7,7 +7,7 @@ from utilities.testing.api import APITestCase
 
 class NetBoxDNSVersionTestCase(SimpleTestCase):
     def test_version(self):
-        assert __version__ == "1.2.9"
+        assert __version__ == "1.2.10"
 
 
 class AppTest(APITestCase):
