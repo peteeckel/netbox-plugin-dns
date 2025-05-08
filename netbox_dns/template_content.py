63f17a0b6430d15b1dbfe8aaed27fd9353ca0877
Peter Eckel <pe-git@hindenburgring.com>
2025-05-08 20:50:52 +0200

    Run configure() on all embedded tables

--- netbox_dns/template_content.py
+++ netbox_dns/template_content.py
@@ -20,6 +20,7 @@ class RelatedDNSRecords(PluginTemplateExtension):
 
     def right_page(self):
         ip_address = self.context.get("object")
+        request = self.context.get("request")
 
         address_records = ip_address.netbox_dns_records.all()
         pointer_records = [
@@ -32,6 +33,7 @@ class RelatedDNSRecords(PluginTemplateExtension):
             address_record_table = RelatedRecordTable(
                 data=address_records,
             )
+            address_record_table.configure(request)
         else:
             address_record_table = None
 
@@ -39,6 +41,7 @@ class RelatedDNSRecords(PluginTemplateExtension):
             pointer_record_table = RelatedRecordTable(
                 data=pointer_records,
             )
+            pointer_record_table.configure(request)
         else:
             pointer_record_table = None
 
@@ -56,11 +59,16 @@ class RelatedDNSViews(PluginTemplateExtension):
 
     def right_page(self):
         prefix = self.context.get("object")
+        request = self.context.get("request")
 
         if assigned_views := prefix.netbox_dns_views.all():
-            context = {"assigned_views": RelatedViewTable(data=assigned_views)}
+            assigned_views_table = RelatedViewTable(data=assigned_views)
+            assigned_views_table.configure(request)
+            context = {"assigned_views": assigned_views_table}
         elif inherited_views := get_views_by_prefix(prefix):
-            context = {"inherited_views": RelatedViewTable(data=inherited_views)}
+            inherited_views_table = RelatedViewTable(data=inherited_views)
+            inherited_views_table.configure(request)
+            context = {"inherited_views": inherited_views_table}
         else:
             context = {}
 
@@ -86,6 +94,7 @@ class IPRelatedDNSRecords(PluginTemplateExtension):
 
     def right_page(self):
         ip_address = self.context.get("object")
+        request = self.context.get("request")
 
         address_records = Record.objects.filter(
             type__in=(RecordTypeChoices.A, RecordTypeChoices.AAAA),
@@ -100,6 +109,7 @@ class IPRelatedDNSRecords(PluginTemplateExtension):
             address_record_table = RelatedRecordTable(
                 data=address_records,
             )
+            address_record_table.configure(request)
         else:
             address_record_table = None
 
@@ -107,6 +117,7 @@ class IPRelatedDNSRecords(PluginTemplateExtension):
             pointer_record_table = RelatedRecordTable(
                 data=pointer_records,
             )
+            pointer_record_table.configure(request)
         else:
             pointer_record_table = None
 
