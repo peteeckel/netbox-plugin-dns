05e5106afcc0e66c1515a05bb0af157cfe6b6d9d
Peter Eckel <pe-git@hindenburgring.com>
2025-07-30 11:03:43 +0200

    Updated the min_version requirement to 4.3.2

--- netbox_dns/__init__.py
+++ netbox_dns/__init__.py
@@ -16,7 +16,7 @@ class DNSConfig(PluginConfig):
     name = "netbox_dns"
     verbose_name = _("NetBox DNS")
     description = _("NetBox plugin for DNS data")
-    min_version = "4.3.0"
+    min_version = "4.3.2"
     version = __version__
     author = "Peter Eckel"
     author_email = "pete@netbox-dns.org"
